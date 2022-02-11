f6d4b6af251bb928824d1d58d447bc3e42924b83
vcz-Yangmae <yangmae@viewcommz.com>
2022-02-11 13:09:49 +0900

    FIX: 스토리북 import 위치 수정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,31 +1,30 @@
-
 // ./storybook/preview.js
 import { addDecorator } from "@storybook/react";
 import { withThemesProvider } from "storybook-addon-styled-component-theme";
 import { ThemeProvider } from "styled-components";
-import theme from '../src/styles/theme';
+import theme from "../src/styles/theme";
 import { MemoryRouter } from "react-router";
 import GlobalStyle from "../src/styles/GlobalStyle";
-import '../src/App.css'; 
+import "../src/app/App.css";
 
-addDecorator(story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>); // router
+addDecorator((story) => <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>); // router
 // addDecorator(withThemesProvider([theme]), ThemeProvider); // style ThemeProvider
 
 export const decorators = [
-  (Story) => (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Story />
-    </ThemeProvider>
-  ),
+    (Story) => (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Story />
+        </ThemeProvider>
+    ),
 ];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+};
